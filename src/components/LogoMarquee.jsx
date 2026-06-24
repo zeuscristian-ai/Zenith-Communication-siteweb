62c0c8f4cbd028530879bce1698d711470c057b2
zeuscristian-ai <cristianyaovia@gmail.com>
2026-06-24 18:51:29 +0000

    Mettre à jour les logos partenaires

--- src/components/LogoMarquee.jsx
+++ src/components/LogoMarquee.jsx
@@ -7,8 +7,8 @@ export default function LogoMarquee() {
     <section className="logo-marquee" aria-label="Références">
       <div className="logo-track">
         {logos.map((logo, index) => (
-          <div className="logo-item" key={`${logo}-${index}`}>
-            <img src={logo} alt="" />
+          <div className="logo-item" key={`${logo.image}-${index}`}>
+            <img src={logo.image} alt={logo.name} />
           </div>
         ))}
       </div>
